b24b3f39512cc287ba57e708cc11056642d907b7
Anton Veldanov <anton.veldanov@gmail.com>
2020-02-01 21:04:11 -0800

    iOS App

---
+++ TestApp/TestApp/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  TestApp
+//
+//  Created by Veldanov, Anton on 2/1/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var name:String
+    init(name:String) {
+        self.name = name
+    }
+}
---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Veldanov, Anton on 2/1/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+        
+
+}
+
