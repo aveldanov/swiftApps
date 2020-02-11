f04a54d226e6be77a9c6b9d67390ba5ed1794fa6
Anton Veldanov <anton.veldanov@gmail.com>
2020-02-11 12:34:47 -0800

    iOS App

---
+++ StructPractice/StructPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StructPractice
+//
+//  Created by Veldanov, Anton on 2/11/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
