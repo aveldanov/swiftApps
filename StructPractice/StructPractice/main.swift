7156d225c8ccb922ef8f11967977d6793fe2c2d4
Anton Veldanov <anton.veldanov@gmail.com>
2020-02-13 16:43:26 -0800

    iOS App

--- StructPractice/StructPractice/main.swift
+++ StructPractice/StructPractice/main.swift
@@ -8,5 +8,58 @@
 
 import Foundation
 
-print("Hello, World!")
+
+struct Town{
+    let name: String
+    var citizens: [String]
+    var resources: [String:Int]
+    
+    init(name:String, citizens:[String],resources:[String:Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    
+    func fortify(){
+        print("Defences Increased!")
+        
+    }
+    
+}
+
+
+var newTown = Town(name: <#T##String#>, citizens: <#T##[String]#>, resources: <#T##[String : Int]#>)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct Town{
+//    let name = "AntonTown"
+//    var citizens = ["cit1","cit2","cit3"]
+//    var resources = ["res1":1,"res2":3,"res3":4]
+//
+//    func fortify(){
+//        print("Defences Increased!")
+//
+//    }
+//
+//}
+
+
 
