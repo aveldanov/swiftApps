71f33f1f2ea3c6605bad2fc1f98cd9d0b520544c
Anton Veldanov <anton.veldanov@gmail.com>
2020-03-08 14:49:07 -0700

    iOS App

---
+++ SturctRev5/SturctRev5/main.swift
@@ -0,0 +1,62 @@
+//
+//  main.swift
+//  SturctRev5
+//
+//  Created by Veldanov, Anton on 3/8/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Town{
+    let name: String
+    var citizens: [String]
+    var resources:[String:Int]
+    func fortify(){
+        print("Defences increased")
+    }
+    init(name:String,citizens:[String], resources:[String:Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+}
+
+
+var myTown = Town(name: "An", citizens: ["John"], resources: ["Coc":3])
+
+print(myTown.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//struct Town{
+//    let name = "AntonTown"
+//    var citizens = ["Anton","Anton2"]
+//    var resources = ["Grain":100, "Ore":42, "Wool":42]
+//    func fortify(){
+//        print("Defences increased")
+//    }
+//}
+//
+//
+//var myTown = Town()
+//
+//print(myTown.citizens)
+//print("My Town \(myTown.name) has \(myTown.resources)")
+//
+//myTown.citizens.append("John")
+//print(myTown.citizens)
+//myTown.fortify()
