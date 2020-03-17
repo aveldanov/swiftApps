7e634a6fa5ca10de0e2cca698cd65f3f51f9bc1e
Anton Veldanov <anton.veldanov@gmail.com>
2020-03-17 09:55:07 -0700

    iOS App

---
+++ Extensions-rev5/Extensions-rev5/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Extensions-rev5
+//
+//  Created by Veldanov, Anton on 3/16/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+
+import Foundation
+
+extension Double{
+    func round(to places:Int)-> Double {
+        let precisionNumber = pow(10, Double(places))
+        var n = self
+        n = n * precisionNumber
+        n.round()
+        n = n / precisionNumber
+        
+        return n
+    }
+}
+
+var myDouble = 3.14159
+
+//let myRoundedDouble = String(format: "%.1f",myDouble)
+
+
+print(myDouble.round(to: 3))
+
+
